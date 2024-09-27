4272da97698bb19c1288058c68fb3dae10a519a6
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-09-27 15:57:52 +0300

    Added assertions to validate responses
    Usually just checking the response status is not
    enough. Validating that the page has loaded correctly
    is also important.

--- scenarios.js
+++ scenarios.js
@@ -4,7 +4,8 @@ import { check } from 'k6';
 export default function () {
   const response = http.get('https://test.k6.io');
   console.log(response.status);
-  check(true, {
-    "True is true": (value) => value === true,
+  check(response, {
+    "Response status is 200": (res) => res.status === 200,
+    "Page is start page": (res) => res.body.includes("Collection of simple web-pages suitable for load testing."),
   });
 }
