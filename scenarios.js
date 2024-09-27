6a1bcf075e2d2e267e4f051920c1baeb6d28a6e7
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-09-27 16:44:17 +0300

    Added thresholds to scenarios.js
    Added thresholds to http_req_duration and http_req_failed

--- scenarios.js
+++ scenarios.js
@@ -1,11 +1,22 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
+
+
+export const options = {
+  vus: 10,
+  duration: '10s',
+  thresholds: {
+    http_req_duration: ['p(95) < 200'],
+    http_req_failed: ['rate < 0.01'], // 1% error rate allowed
+  }
+}
 
 export default function () {
   const response = http.get('https://test.k6.io');
-  console.log(response.status);
+  // console.log(response.status);
   check(response, {
     "Response status is 200": (res) => res.status === 200,
     "Page is start page": (res) => res.body.includes("Collection of simple web-pages suitable for load testing."),
   });
+  sleep(2);
 }
