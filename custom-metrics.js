53ff75286622a4d5e550995b7a83ff2e6f44af93
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-09-30 12:58:24 +0300

    Added more custom metrics
    Added a custom metric to check the response time of the news page.
    Also added two separate thresholds for the custom metric.

---
+++ custom-metrics.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import exec from 'k6/execution';
+import { Counter, Trend } from 'k6/metrics'; // Counter, Rate, Gauge,  Trend
+
+let myCounter = new Counter('my_counter');
+let newsPageResponseTrend = new Trend('response_time_news_page');
+
+export const options = {
+  vus: 5,
+  duration: '5s',
+  thresholds: {
+    my_counter: ['count > 4'],
+    response_time_news_page: ['p(95) < 200'],
+    response_time_news_page: ['p(99) < 250'],
+  }
+}
+
+export default function () {
+  let response = http.get("https://test.k6.io");
+  myCounter.add(1);
+  sleep(1);
+
+  response = http.get("https://test.k6.io/news");
+  newsPageResponseTrend.add(response.timings.duration);
+  sleep(1);
+}
