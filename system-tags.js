9dea636f4fe9d4851a3d9f9dab39db649eec9ec3
aarneharju <35223549+aarneharju@users.noreply.github.com>
2024-09-30 16:23:54 +0300

    Added system tags

---
+++ system-tags.js
@@ -0,0 +1,14 @@
+import http from 'k6/http';
+
+export const options = {
+  thresholds: {
+    http_req_duration: ['p(95) < 1000'],
+    'http_req_duration{status: 200}': ['p(95) < 1000'],
+    'http_req_duration{status: 201}': ['p(95) < 1000'],
+  }
+}
+
+export default function () {
+  http.get('https://run.mocky.io/v3/25b5c374-97b2-4b1b-9b0c-214e9708693b');
+  http.get('https://run.mocky.io/v3/869f0325-d96d-4fea-ac09-7a0856b46963?mocky-delay=2000ms');
+}
