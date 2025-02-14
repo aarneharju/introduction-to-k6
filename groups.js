47b682483e6cd69f1110cbdf3a025bde9b311f2f
aarneharju <35223549+aarneharju@users.noreply.github.com>
2025-02-14 17:04:40 +0200

    Added detailed group duration metrics

--- groups.js
+++ groups.js
@@ -4,6 +4,8 @@ import { sleep, check, group } from "k6";
 export const options = {
   thresholds: {
     http_req_duration: ["p(95) < 250"],
+    "group_duration{group:::Main page}": ["p(95) < 200"],
+    "group_duration{group:::Main page::Assets}": ["p(95) < 200"],
   },
 };
 
