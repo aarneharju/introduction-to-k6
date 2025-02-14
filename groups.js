cf0464dbb89c68f180a175cece4128763278b717
aarneharju <35223549+aarneharju@users.noreply.github.com>
2025-02-14 18:02:16 +0200

    Updated groups.js with mocky.io urls with delays

--- groups.js
+++ groups.js
@@ -3,25 +3,34 @@ import { sleep, check, group } from "k6";
 
 export const options = {
   thresholds: {
-    http_req_duration: ["p(95) < 250"],
-    "group_duration{group:::Main page}": ["p(95) < 200"],
-    "group_duration{group:::Main page::Assets}": ["p(95) < 200"],
+    http_req_duration: ["p(95) < 5500"],
+    "group_duration{group:::Main page}": ["p(95) < 8000"],
+    "group_duration{group:::Main page::Assets}": ["p(95) < 3000"],
+    "group_duration{group:::News page}": ["p(95) < 6000"],
   },
 };
 
 export default function () {
   group("Main page", function () {
-    let response = http.get("https://test.k6.io/");
+    let response = http.get(
+      "https://run.mocky.io/v3/7e4d2a14-1401-4337-be84-884299ef4cab?mocky-delay=5000ms"
+    );
     check(response, { "status is 200": (res) => res.status === 200 });
 
     group("Assets", function () {
-      http.get("https://test.k6.io/static/css/site.css");
-      http.get("https://test.k6.io/static/js/prisms.js");
+      http.get(
+        "https://run.mocky.io/v3/7e4d2a14-1401-4337-be84-884299ef4cab?mocky-delay=1000ms"
+      );
+      http.get(
+        "https://run.mocky.io/v3/7e4d2a14-1401-4337-be84-884299ef4cab?mocky-delay=1000ms"
+      );
     });
   });
 
   group("News page", function () {
-    http.get("https://test.k6.io/news");
+    http.get(
+      "https://run.mocky.io/v3/7e4d2a14-1401-4337-be84-884299ef4cab?mocky-delay=5000ms"
+    );
   });
   sleep(1);
 }
