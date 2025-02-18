c9ca538db637b0af3439d15b7586f9088f1c7237
aarneharju <35223549+aarneharju@users.noreply.github.com>
2025-02-18 18:48:15 +0200

    Added assignment-03-retrieve-a-resource-after-creation.js and added numbering to the assignments.

---
+++ assignment-03-retrieve-a-resource-after-creation.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+
+export default function () {
+    const bodyLogin = JSON.stringify({
+        username: "userwithafairlylongname",
+        password: "wordofpassing",
+    });
+
+    const paramsLogin = {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+
+    const responseLogin = http.post("https://test-api.k6.io/auth/token/login/", bodyLogin, paramsLogin);
+
+    console.log(responseLogin.json().access);
+    const accessToken = responseLogin.json().access;
+
+    const paramsToken = {
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        }
+    }
+
+    const responseCroc = http.get("https://test-api.k6.io/my/crocodiles/15520941/", paramsToken);
+    console.log(responseCroc.json());
+}
